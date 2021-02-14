95b2f8723d06f812dc4302441f4dafb004494ece
Routmoute <tom@yourlabs.org>
2021-02-14 02:04:51 +0100

    add tests

---
+++ tests/Unit/Service/RoutmouteDiscordApiServiceTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Routmoute\Bundle\RoutmouteDiscordBundle\Tests\Unit\Service;
+
+use PHPUnit\Framework\TestCase;
+use Routmoute\Bundle\RoutmouteDiscordBundle\Service\RoutmouteDiscordApiService;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+
+class RoutmouteDiscordApiServiceTest extends TestCase
+{
+    private $botToken;
+    private $discordId;
+    private $guildId;
+
+    public function registerBundles()
+    {
+        return [
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Routmoute\Bundle\RoutmouteDiscordBundle\RoutmouteDiscordBundle(),
+        ];
+    }
+
+    public function testGetUserFromDiscordId()
+    {
+        $this->botToken = $this->generateRandomString();
+        $this->discordId = $this->generateRandomString();
+
+        $mockHttpClient = new MockHttpClient(function($method, $url, $options) {
+            $this->assertEquals("GET", $method);
+            $this->assertEquals('https://discord.com/api/users/' . $this->discordId, $url);
+            $this->assertEquals($options['headers'][1], "Authorization: Bot " . $this->botToken);
+
+            return new MockResponse(json_encode([]), ['headers' => ['Content-Type' => 'application/json']]);
+        });
+
+        $apiService = new RoutmouteDiscordApiService($this->botToken, $mockHttpClient);
+
+        $apiService->getUserFromDiscordId($this->discordId);
+    }
+
+    public function testGetUserFromGuild()
+    {
+        $this->botToken = $this->generateRandomString();
+        $this->discordId = $this->generateRandomString();
+        $this->guildId = $this->generateRandomString();
+
+        $mockHttpClient = new MockHttpClient(function($method, $url, $options) {
+            $this->assertEquals("GET", $method);
+            $this->assertEquals("https://discord.com/api/guilds/" . $this->guildId . "/members/" . $this->discordId, $url);
+            $this->assertEquals($options['headers'][1], "Authorization: Bot " . $this->botToken);
+
+            return new MockResponse(json_encode([]), ['headers' => ['Content-Type' => 'application/json']]);
+        });
+
+        $apiService = new RoutmouteDiscordApiService($this->botToken, $mockHttpClient);
+
+        $apiService->getUserFromGuild($this->guildId, $this->discordId);
+    }
+
+    private function generateRandomString($length = 50) {
+        return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+    }
+}
