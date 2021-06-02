b1e3afaa0967df063d3a881f647c6fac2d0bdfcf
Routmoute <tom@yourlabs.org>
2021-06-02 19:58:49 +0200

    add getChannelMessages function

--- src/Service/RoutmouteDiscordApiService.php
+++ src/Service/RoutmouteDiscordApiService.php
@@ -65,6 +65,33 @@ class RoutmouteDiscordApiService
         return $apiResponse->toArray();
     }
 
+    /**
+     *
+     * @param string $channelId
+     * @param integer $limit default 50
+     * @param string|null $around Message Id
+     * @param string|null $before Message Id
+     * @param string|null $after Message Id
+     * @return array of messages
+     */
+    public function getChannelMessages(string $channelId, int $limit = 50, ?string $around, ?string $before, ?string $after): array
+    {
+        $addOptions = "?limit=" . $limit;
+        if ($around) $addOptions += "&around=" . $around;
+        if ($before) $addOptions += "&before=" . $before;
+        if ($before) $addOptions += "&after=" . $after;
+        $apiResponse = $this->sendToDiscordApi('GET', self::DISCORD_API . '/channels/' . $channelId . '/messages' . $addOptions);
+        $statusCode = $apiResponse->getStatusCode();
+        if ($statusCode != 200)
+        {
+            if ($statusCode == 404) {
+                throw new \Exception('Channel not exist', 404);
+            }
+            throw new DiscordAccessFailedException();
+        }
+        return $apiResponse->toArray();
+    }
+
     private function sendToDiscordApi(string $requestType, string $endPoint): ResponseInterface
     {
         return $this->httpClient->request($requestType, $endPoint, [
