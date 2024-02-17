4b82a7fcf34155bb6516289da5fddf2830889eec
Joel Conceição <joelreis@webtejo.pt>
2024-02-17 15:06:23 +0000

    Fixed Void deprecation

--- src/DependencyInjection/RoutmouteDiscordExtension.php
+++ src/DependencyInjection/RoutmouteDiscordExtension.php
@@ -7,17 +7,18 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class RoutmouteDiscordExtension extends Extension {
-    public function load(array $configs, ContainerBuilder $container)
+class RoutmouteDiscordExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        
+
         $scope = '';
-        foreach($config['oauth']['scope'] as $value) {
+        foreach ($config['oauth']['scope'] as $value) {
             if ($scope != '') {
                 $scope .= ' ';
             }
